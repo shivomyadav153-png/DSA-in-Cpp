35afda4960090d9d4c4dd0085a4efda1da5a1250
Shivom yadav <shivomyadav153@gmail.com>
2026-03-22 12:05:07 +0530

    Implement string reversal in main function
    Fix syntax error in reverse function call.

---
+++ String.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <algorithm> 
+
+using namespace std;
+
+int main() {
+  string str = "apna college";
+  reverse(str.begin(), str.end(); //iterators
+  cout << str <<endl;
+ return 0;
+}
