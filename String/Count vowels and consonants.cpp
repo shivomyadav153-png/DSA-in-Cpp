b28ef10913831d802cb000871b342bb5894aa718
Shivom yadav <shivomyadav153@gmail.com>
2026-03-23 23:01:36 +0530

    Count vowels and consonants in a string
    Fix variable name typo and correct character case function.

---
+++ String/Count vowels and consonants.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string str = "hello world";
+int vowels = 0, constants = 0;
+
+for(int i = 0; i<str.length(); i++) {
+char ch = tolowe (str[i]);
+if(ch >= 'a' && ch <= 'z') {
+if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+  vowels++;
+else
+  consonants++:
+}
+}
+cout << "vowels: " << vowels <<endl;
+cout << "consonants: " << consonants;
+return 0;
+}
