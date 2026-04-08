79739985d356c122601014e9fdf00c5cacdc9afb
Shivom yadav <shivomyadav153@gmail.com>
2026-04-08 20:36:24 +0530

    Add program to find duplicate elements in an array

---
+++ Arrays /Duplicate element find.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5];
+
+    cout << "Enter 5 elements: ";
+    for(int i = 0; i < 5; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Duplicate elements: ";
+    for(int i = 0; i < 5; i++) {
+        for(int j = i+1; j < 5; j++) {
+            if(arr[i] == arr[j]) {
+                cout << arr[i] << " ";
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
