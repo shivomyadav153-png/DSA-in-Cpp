669620405a987bd6f9cdf2e36a5f52e8b57a03e6
Shivom yadav <shivomyadav153@gmail.com>
2026-04-10 23:55:30 +0530

    Implement matrix addition in C++

---
+++ Arrays /Matrix addition.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[2][2], b[2][2], sum[2][2];
+
+    cout << "Enter first matrix:\n";
+    for(int i=0;i<2;i++)
+        for(int j=0;j<2;j++)
+            cin >> a[i][j];
+
+    cout << "Enter second matrix:\n";
+    for(int i=0;i<2;i++)
+        for(int j=0;j<2;j++)
+            cin >> b[i][j];
+
+    for(int i=0;i<2;i++)
+        for(int j=0;j<2;j++)
+            sum[i][j] = a[i][j] + b[i][j];
+
+    cout << "Sum Matrix:\n";
+    for(int i=0;i<2;i++) {
+        for(int j=0;j<2;j++)
+            cout << sum[i][j] << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
